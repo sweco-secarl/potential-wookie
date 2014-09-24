0c81ae76ea81bf7ff6d4b03ebdf3184a71dd9682
hochas <jofaan@gmail.com>
2014-09-25 00:30:13 +0200

    first commit

---
+++ gruntfile.js
@@ -0,0 +1,47 @@
+// Gruntfile with the configuration of grunt-express and grunt-open. No livereload yet!
+module.exports = function(grunt) {
+
+    // Load Grunt tasks declared in the package.json file
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // Configure Grunt
+    grunt.initConfig({
+
+        // grunt-express will serve the files from the folders listed in `bases`
+        // on specified `port` and `hostname`
+        express: {
+            all: {
+                options: {
+                    port: 9000,
+                    hostname: "0.0.0.0",
+                    bases: ['app']
+                }
+            }
+        },
+
+        // grunt-open will open your browser at the project's URL
+        open: {
+            all: {
+                // Gets the port from the connect configuration
+                path: 'http://localhost:<%= express.all.options.port%>'
+            }
+        },
+
+        wiredep: {
+            target: {
+                // Point to the files that should be updated when
+                // you run `grunt wiredep`
+                src: [
+                    'app/index.html'
+                ]
+            }
+        }
+    });
+
+    // Creates the `server` task
+    grunt.registerTask('server', [
+        'express',
+        'open',
+        'express-keepalive'
+    ]);
+};
